6aaccd089098e7538a85af1befff928573b89b50
Chris Murphy <cmurphy@thewebadvisors.ca>
2022-02-20 22:33:59 -0500

    Changed asset namespace

--- src/Extension/LeftAndMainExtension.php
+++ src/Extension/LeftAndMainExtension.php
@@ -8,9 +8,8 @@ use SilverStripe\View\Requirements;
 class LeftAndMainExtension extends Extension {
     public function init() {
         // @todo load these from the config.yml
-        Requirements::javascript('_resources/app/_resources/dist/assets/main.js',    ['type'=>'module']); // Generate the type="module" attribute
-        Requirements::javascript('_resources/app/_resources/dist/assets/vendor.js',  ['type'=>'module']); // Generate the type="module" attribute
-        Requirements::javascript('_resources/app/_resources/dist/assets/plugins.js', ['type'=>'module']); // Generate the type="module" attribute
+        Requirements::javascript('_resources/app/_resources/dist/assets/rs-md-colorfield.js',    ['type'=>'module']); // Generate the type="module" attribute
+        Requirements::javascript('_resources/app/_resources/dist/assets/rs-vendor.js',  ['type'=>'module']); // Generate the type="module" attribute
 
         Requirements::css('_resources/app/_resources/dist/assets/main.css');
     }
